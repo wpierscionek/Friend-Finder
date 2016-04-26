2682ae4e0a8c2aadb7954eda62796110f3b37a14
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-04-26 10:33:53 -0400

    4/26/2016

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,1 +1,16 @@
-var tableData = require('../data/table-data.js');
+var tableData = require('../data/friends.js');
+var path = require('path');
+
+module.exports = function(app){
+
+	app.get('/api/survey', function(request, response){
+		res.json(friends);
+	});
+
+	app.post('/api/survey', function(request, response){
+		if(friends.length = 10)	{
+			friends.push(request.body);
+			request.json(true);
+		}
+	})
+}
