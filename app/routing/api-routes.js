240afde4707b48b677d1060b6e22fb59a4b1e4d5
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-04-29 22:49:20 -0400

    4/29/2016

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -11,19 +11,38 @@ var path = require('path');
 
 //need to make routing available to rest of the application
 
-module.exports = function(app){
-	// Below API GET REQUESTS
-	// handles when user "visit" a page
-	// In each of the below cases when a user visits a link
+module.exports = function(app) {
+    // Below API GET REQUESTS
+    // handles when user "visit" a page
+    // In each of the below cases when a user visits a link
 
-	app.get('/api/survey', function(request, response){
-		response.json(usersData);
-	});
+    // app.get('/api/survey', function(request, response) {
+    //     response.json(usersData);
+    // });
 
-	//Below API POST REQUESTS
-	app.post('/api/survey', function(request, response){
-		usersData.push(request);
-		response.json(true); // KEY LINE
-	})
+    // Below API POST REQUESTS
+    app.post('/api/survey', function(request, response) {
+    	
+    	usersData.push(request.body);
+
+        // console.log(request.body.name);
+        // console.log(request.body.scores.length);
+
+        for (var i = 0; i < usersData.length; i++) {
+
+                var sum = usersData[i].scores.reduce(addScore);
+
+                function addScore(a, b) {
+                    return a + b;
+                }
+                console.log(sum);
+
+                // response.json({
+                // 	'name':
+                // 	'photo':
+                // })
+
+        };
+
+    })
 }
-
